ba35f001350573965f7b2a46291747717a8b995b
Jungwook Park <cco2416@gmail.com>
2021-03-20 13:32:03 +0900

    Solve CodeUp ~1029

---
+++ src/codeup/codeup1020to1039/CodeUp1021.java
@@ -0,0 +1,13 @@
+package codeup.codeup1020to1039;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodeUp1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s);
+    }
+}
