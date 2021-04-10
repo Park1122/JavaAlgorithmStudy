b3b7ec01a1bd2a6c92c47dd3a9d405e80a60820d
Jungwook Park <cco2416@gmail.com>
2021-04-10 21:28:17 +0900

    Solve SWEA2050

---
+++ src/swea/d1/Swea2050.java
@@ -0,0 +1,20 @@
+package swea.d1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Swea2050 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String str = br.readLine();
+            for (int i = 0; i < str.length(); i++) { // 'A' = 65 ~ 'Z' = 90
+                System.out.print((int)(str.charAt(i)) - 64);
+                System.out.print(" ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
