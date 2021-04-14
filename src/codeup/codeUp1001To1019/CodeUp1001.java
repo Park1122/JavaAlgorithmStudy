2019111610d77d2472f533eb2aa53d231b96ecbd
Jungwook Park <cco2416@gmail.com>
2021-04-14 14:43:12 +0900

    Apply .gitignore

--- src/codeup/codeUp1001To1019/CodeUp1001.java
+++
@@ -1,7 +0,0 @@
-package codeup.codeUp1001To1019;
-
-public class CodeUp1001 {
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
-}
--- src/codeup/codeUp1001To1019/CodeUp1003.java
+++
@@ -1,8 +0,0 @@
-package codeup.codeUp1001To1019;
-
-public class CodeUp1003 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("World");
-    }
-}
