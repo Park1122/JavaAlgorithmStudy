a6dfa4c8f0d0c6fd7d85a39d2b2c3cd2812c7fe0
Jungwook Park <cco2416@gmail.com>
2021-05-06 23:37:53 +0900

    Learn queue

---
+++ src/fastcampus/datastructure/MyQueue.java
@@ -0,0 +1,40 @@
+package fastcampus.datastructure;
+
+import java.util.ArrayList;
+
+public class MyQueue<T> {
+    ArrayList<T> queue = new ArrayList<>();
+
+    public void enqueue(T item) {
+        queue.add(item);
+    }
+
+    public T dequeue() {
+        if (queue.isEmpty()) {
+            return null;
+        } else {
+            return queue.remove(0);
+        }
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        // 실제 java 에서 Queue 의 경우, Queue 로 선언 후, LinkedList 로 생성하여 사용한다.
+        // enqueue -> add, offer 메소드 사용
+        // dequeue -> remove, poll 메소드 사용
+        MyQueue<Integer> queue = new MyQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        System.out.println(queue.isEmpty());
+    }
+}
