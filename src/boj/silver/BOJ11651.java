43359d785ff8cb24a69f3a22d5463ebc09f6f0ce
Jungwook Park <cco2416@gmail.com>
2021-06-16 19:29:02 +0900

    Solve BOJ11651

---
+++ src/boj/silver/BOJ11651.java
@@ -0,0 +1,40 @@
+package boj.silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int tc = Integer.parseInt(br.readLine());
+        int[][] points = new int[tc][2];
+        StringTokenizer st;
+
+        for (int i = 0; i < tc; i++) {
+            st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        for (int i = 0; i < tc; i++) {
+            bw.write(points[i][0] + " " + points[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
