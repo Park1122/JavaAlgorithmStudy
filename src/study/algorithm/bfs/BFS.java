b5a21a79e1e602f9b7412a12ec7a3749995adcac
Jungwook Park <cco2416@gmail.com>
2021-06-26 18:14:11 +0900

    Learn BFS

---
+++ src/study/algorithm/bfs/BFS.java
@@ -0,0 +1,47 @@
+package study.algorithm.bfs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BFS {
+    public ArrayList<String> bfsFunc(HashMap<String, ArrayList<String>> graph, String startNode) {
+        ArrayList<String> visited = new ArrayList<>();
+        ArrayList<String> needVisit = new ArrayList<>();
+
+        needVisit.add(startNode);
+
+        while (needVisit.size() > 0) {
+            String node = needVisit.remove(0);
+
+            if (!visited.contains(node)) {
+                visited.add(node);
+                needVisit.addAll(graph.get(node));
+            }
+        }
+
+        return visited; // 탐색(방문) 순서가 적혀있는 리스트 반환
+    }
+
+    public static void main(String[] args) {
+        //create graph -> use HashMap && ArrayList
+        HashMap<String, ArrayList<String>> graph = new HashMap<>();
+        graph.put("A", new ArrayList<>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<>(Arrays.asList("I")));
+
+        //bfs
+        BFS bfs = new BFS();
+        ArrayList<String> bfsSequence = bfs.bfsFunc(graph, "A");
+
+        //print
+        System.out.println(bfsSequence);
+    }
+}
