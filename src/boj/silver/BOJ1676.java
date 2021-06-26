f3c64d6cd7c2b9bbe91c9b0c201c0da99d4e77eb
Jungwook Park <cco2416@gmail.com>
2021-06-26 16:18:09 +0900

    Solve BOJ1676

---
+++ src/boj/silver/BOJ1676.java
@@ -0,0 +1,23 @@
+package boj.silver;
+
+import java.io.*;
+
+public class BOJ1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int two = 0;
+        int five = 0;
+        for (int i = 2; i <= n; i*=2) {
+            two += n/i;
+        }
+        for (int i = 5; i <= n; i *= 5) {
+            five += n/i;
+        }
+        bw.write(Math.min(two, five) + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
