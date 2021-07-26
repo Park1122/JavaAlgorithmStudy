164ba5caae8d50ca4ca8a641e7bff2b472ce13fc
Jungwook Park <cco2416@gmail.com>
2021-07-26 21:52:26 +0900

    Solve BOJ1260

---
+++ src/boj/silver/BOJ1260.java
@@ -0,0 +1,77 @@
+package boj.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1260 {
+    static int N, M, V;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+    }
+
+    static void dfs(int x) {
+        visited[x] = true;
+        sb.append(x).append(' ');
+
+        for (int y : graph[x]) {
+            if(visited[y]) continue;
+            dfs(y);
+        }
+    }
+
+    static void bfs(int x) {
+        Queue<Integer> que = new LinkedList<>();
+
+        que.add(x);
+        visited[x] = true;
+
+        while (!que.isEmpty()) {
+            int node = que.poll();
+
+            sb.append(node).append(' ');
+            for (int y : graph[node]) {
+                if (visited[y]) continue;
+
+                que.add(y);
+                visited[y] = true;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        sb.append('\n');
+        for (int i = 1; i <= N; i++) {
+            visited[i] = false;
+        }
+        bfs(V);
+        System.out.println(sb);
+    }
+}
