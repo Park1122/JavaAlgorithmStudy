c6cdbbe9cab0d20737ee47f80902ac3057215d8f
Jungwook Park <cco2416@gmail.com>
2021-08-01 20:07:04 +0900

    Solve BOJ1929

--- src/boj/silver/BOJ1929.java
+++ src/boj/silver/BOJ1929.java
@@ -1,38 +1,47 @@
 package boj.silver;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1929 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        boolean[] primes = new boolean[n+1]; // 0 ~ n
+    static int M, N;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] primes;
 
-        // false = 실수
-        primes[0] = true;
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        primes = new boolean[N + 1];
+    }
+
+    static void getPrime() {
         primes[1] = true;
 
-        for (int i = 2; i < Math.sqrt(n); i++) {
-            if(primes[i] == true) continue; // 배수로 체크가 된 경우는 넘어감
-            for (int j = i * i; j < primes.length; j = j+i) {
-                primes[j] = true;
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; i * j <= N; j++) {
+                primes[i*j] = true;
             }
         }
+    }
 
-        for (int i = m; i <= n; i++) {
+    static void solve() {
+        getPrime();
+
+        for (int i = M; i <= N; i++) {
             if (!primes[i]) {
-                sb.append(i).append("\n");
+                sb.append(i).append('\n');
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
     }
 }
