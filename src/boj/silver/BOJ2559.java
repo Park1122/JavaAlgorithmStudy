b17aa40369f77003fe7c6e4b6b7551419ad70207
Park Jungwook <cco2416@gmail.com>
2022-04-01 14:21:26 +0900

    Solve BOJ2559

--- src/boj/silver/BOJ2559.java
+++ src/boj/silver/BOJ2559.java
@@ -7,31 +7,35 @@ import java.util.StringTokenizer;
 
 public class BOJ2559 {
     static int N, K;
-    static int[] a;
+    static int[] A;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
     static void input() throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        a = new int[N + 1];
+        A = new int[N + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
     }
 
-    static void solve() {
-        int R = 0, sum = 0, ans = -100 * N;
+    static void pro() {
+        int R = 0;
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+
         for (int L = 1; L + K - 1 <= N; L++) {
-            sum -= a[L - 1];
+            sum -= A[L - 1];
 
             while (R + 1 <= L + K - 1) {
                 R++;
-                sum += a[R];
+                sum += A[R];
             }
-            ans = Math.max(ans, sum);
+            ans = Math.max(sum, ans);
         }
 
         System.out.println(ans);
@@ -39,6 +43,6 @@ public class BOJ2559 {
 
     public static void main(String[] args) throws IOException {
         input();
-        solve();
+        pro();
     }
 }
